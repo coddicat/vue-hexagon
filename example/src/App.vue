74348c8e0a0d0a6b22931ae8006cffcfe3cf52ab
CoddiCat <coddicat@gmail.com>
2021-07-09 16:15:07 +0300

    src with example app

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,18 +1,106 @@
 <template>
   <div id="app">
-    Hello world
+    <div class="column">
+      <Hexagon @click="onClick('#1')">
+        Default
+      </Hexagon>
+      <Hexagon
+        @click="onClick('#2')"
+        :background-image="`url(https://picsum.photos/id/10/100/100)`"
+      >
+        <span style="color: #0ff; text-align: center">Background Image</span>
+      </Hexagon>
+      <Hexagon
+        @click="onClick('#3')"
+        :border-size="10"
+        :background-image="`url(https://picsum.photos/id/20/100/100)`"
+      >
+        <span style="color: #f0f">Border</span>
+      </Hexagon>
+    </div>
+    <div class="column">
+      <Hexagon
+        @click="onClick('#4')"
+        :content-style="{ transform: 'rotate(45deg)' }"
+      >
+        Content Style
+      </Hexagon>
+      <Hexagon @click="onClick('#5')" background-color="red">
+        <span style="text-align: center">Background Color</span>
+      </Hexagon>
+      <Hexagon
+        @click="onClick('#6')"
+        :size="150"
+        class="hexagon-custom"
+        style="background-color: #ccc"
+      >
+        <span style="text-align: center">Custom Class &amp; Style</span>
+      </Hexagon>
+    </div>
+    <div class="column">
+      <Hexagon
+        :size="200"
+        @mousemove="(e) => onMouseEvent('mousemove', e)"
+        @mouseover="(e) => onMouseEvent('mouseover', e)"
+        @mouseout="(e) => onMouseEvent('mouseout', e)"
+        @mousedown="(e) => onMouseEvent('mousedown', e)"
+        @mouseup="(e) => onMouseEvent('mouseup', e)"
+        @mousewheel="(e) => onMouseEvent('mousewheel', e)"
+        @mouseenter="(e) => onMouseEvent('mouseenter', e)"
+        @mouseleave="(e) => onMouseEvent('mouseleave', e)"
+      >
+        <span style="text-align: center">Event Handlers</span>
+      </Hexagon>
+      Event:
+      {{ eventName }}<br />
+      Data: {{ eventData }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
+//import Hexagon from "./dest/hexagon.vue";
 export default Vue.extend({
   components: {
+    //Hexagon,
   },
   data: () => ({
+    eventName: "N/A",
+    eventData: {},
   }),
   methods: {
+    onClick(text: string): void {
+      alert(`clicked: ${text}`);
+    },
+    onMouseEvent(name: string, evt: MouseEvent): void {
+      this.eventName = name;
+      this.eventData = {
+        clientX: evt.clientX,
+        clientY: evt.clientY,
+        offsetX: evt.offsetX,
+        offsetY: evt.offsetY,
+        type: evt.type,
+        target: evt.target
+      };
+      console.log(evt);
+    },
   },
 });
 </script>
+
+<style lang="scss">
+.column {
+  display: inline-block;
+  margin-right: 50px;
+  vertical-align: top;
+  max-width: 250px;
+}
+.hexagon-custom {
+  border-radius: 50%;
+  opacity: 0.5;
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
---
+++ src/hexagon.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="coddicat_hexagon__wrapper" :style="wrapperStyle">
+    <div
+      class="coddicat_hexagon__border coddicat_hexagon"
+      :style="borderStyles"
+    ></div>
+    <div
+      :class="contentClass"
+      class="coddicat_hexagon__content coddicat_hexagon"
+      :style="contentStyles"
+      @click="($e) => $emit('click', $e)"
+      @mousedown="($e) => $emit('mousedown', $e)"
+      @mouseup="($e) => $emit('mouseup', $e)"
+      @mousewheel="($e) => $emit('mousewheel', $e)"
+      @mousemove="($e) => $emit('mousemove', $e)"
+      @mouseenter="($e) => $emit('mouseenter', $e)"
+      @mouseover="($e) => $emit('mouseover', $e)"
+      @mouseleave="($e) => $emit('mouseleave', $e)"
+      @mouseout="($e) => $emit('mouseout', $e)"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default /*#__PURE__*/ Vue.extend({
+  name: "hexagon",
+  props: {
+    borderSize: {
+      type: Number,
+      default: 1.5,
+    },
+    borderColor: {
+      type: String,
+      default: "#000",
+    },
+    size: {
+      type: Number,
+      default: 100,
+    },
+    backgroundColor: {
+      type: String,
+      default: "#fff",
+    },
+    backgroundImage: {
+      type: String,
+      default: undefined,
+    },
+    contentStyle: {
+      type: Object,
+      default: undefined,
+    },
+    contentClass: {
+      type: String,
+      default: undefined,
+    },
+  },
+  computed: {
+    wrapperStyle(): any {
+      const px = `${this.size}px`;
+      return {
+        width: px,
+        height: px,
+      };
+    },
+    contentStyles(): any {
+      const px = `${this.size}px`;
+      const innerStyles = {
+        width: px,
+        height: px,
+        "background-color": this.backgroundColor,
+        "background-image": this.backgroundImage,
+      };
+      return { ...innerStyles, ...this.contentStyle };
+    },
+    borderStyles(): any {
+      const px = `${-this.borderSize}px`;
+      return {
+        top: px,
+        right: px,
+        bottom: px,
+        left: px,
+        "background-color": this.borderColor,
+      };
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.coddicat_hexagon {
+  content: "";
+  position: absolute;
+  display: block;
+  clip-path: polygon(
+    0% 50%,
+    25% 6.7%,
+    75% 6.7%,
+    100% 50%,
+    75% 93.3%,
+    25% 93.3%
+  );
+  background-size: contain;
+  margin: auto;
+  user-select: none;
+
+  &__content {
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    user-select: none;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  &__wrapper {
+    position: relative;
+    //margin: auto;
+    //transform: translate(-50%, -50%);
+    user-select: none;
+  }
+}
+</style>
